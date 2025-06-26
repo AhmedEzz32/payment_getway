b011e89666cf43f46c1ca0d3dc2f68209a1ba5c6
Ahmed Ezz <132682073+AhmedEzz32@users.noreply.github.com>
2025-06-26 20:51:20 +0300

    Add reusable text styles in Styles class
    Introduced a Styles abstract class in styles.dart with predefined static TextStyle instances for consistent typography across the app.

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle style25 = const TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    height: 0,
+  );
+
+  static TextStyle style18 = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    height: 0,
+  );
+
+  static TextStyle styleBold18 = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    height: 0,
+  );
+
+  static TextStyle style24 = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    height: 0,
+  );
+
+  static TextStyle style20 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+    color: Colors.black.withAlpha(204),
+    height: 0,
+  );
+  
+}
