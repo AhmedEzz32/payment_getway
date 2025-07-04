5a13f1e55c51bcb991c50f179f9c85d92c07c575
Ahmed Ezz <132682073+AhmedEzz32@users.noreply.github.com>
2025-07-04 18:20:00 +0300

    Add assets and update test to use MyCardView
    Added my_cart.png image asset and generated assets.dart for asset references. Removed the old my_card_view.dart and updated widget_test.dart to use MyCardView from the new location. Updated pubspec.yaml to include the new asset and asset generation configuration.

---
+++ lib/core/assets.dart/assets.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesMyCart
+  /// assets/images/my_cart.png
+  static const String assetsImagesMyCart = "assets/images/my_cart.png";
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:payment_getway/main.dart';
+import 'package:payment_getway/features/checkout/persentation/views/my_card_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyCardView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
